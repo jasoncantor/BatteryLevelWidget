fca75045ca560f27d245abfb3ab65122b020281a
Jason Cantor <46542133+jasoncantor@users.noreply.github.com>
2024-02-19 21:40:56 -0700

    Initial Commit

---
+++ BatteryLevelWidget/BatteryLevelWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryLevelWidgetApp.swift
+//  BatteryLevelWidget
+//
+//  Created by Jason Cantor on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct BatteryLevelWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
